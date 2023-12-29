cfca59f32fb18a9976362a92eff0e95b23577585
unknown <imamovic.kerim@gmail.com>
2023-12-29 15:43:25 +0100

    izmjene 3 - 03_section
    -kreiranje foldera 03_section
    -instaliranje axios paketa u terminalu

---
+++ 03_section/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import { fetchUsers } from './features/users/usersSlice';
+
+store.dispatch(fetchUsers());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
